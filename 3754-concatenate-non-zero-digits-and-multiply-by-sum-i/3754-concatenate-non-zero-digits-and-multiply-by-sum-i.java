a68cd69cd9335d5ac9d5cdbf8fc86ed1a137a6ba
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-07-07 09:02:21 +0530

    Time: 1 ms (99.85%), Space: 42.5 MB (83.11%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,24 +1,44 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        long nonZero = reverseWithoutZero(n);
-        long temp = nonZero;
+        long nonzero =0; 
         long sum = 0;
-        while (temp > 0) {
-            sum += temp % 10;
-            temp /= 10;
+        int i=1;
+        while (n > 0) {
+            long digit=n%10;
+            n/=10;
+            if(digit==0) continue;
+            sum+=digit;
+            nonzero=nonzero+(digit*i);
+
+            i*=10;
         }
-        long reverseAgain = reverseWithoutZero(nonZero);
-        return reverseAgain * sum;
-    }
-    private long reverseWithoutZero(long num) {
-        long reverse = 0;
-        while (num > 0) {
-            long digit = num % 10;
-            if (digit != 0) {
-                reverse = reverse * 10 + digit;
-            }
-            num /= 10;
-        }
-        return reverse;
+        return nonzero*sum;
     }
 }
+
+
+
+// class Solution {
+//     public long sumAndMultiply(int n) {
+//         long nonZero = reverseWithoutZero(n);
+//         long temp = nonZero;
+//         long sum = 0;
+//         while (temp > 0) {
+//             sum += temp % 10;
+//             temp /= 10;
+//         }
+//         long reverseAgain = reverseWithoutZero(nonZero);
+//         return reverseAgain * sum;
+//     }
+//     private long reverseWithoutZero(long num) {
+//         long reverse = 0;
+//         while (num > 0) {
+//             long digit = num % 10;
+//             if (digit != 0) {
+//                 reverse = reverse * 10 + digit;
+//             }
+//             num /= 10;
+//         }
+//         return reverse;
+//     }
+// }
