bcf062ffe81e901d46aea767579109a06bd5f93b
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-07-20 16:33:55 +0530

    Time: 18 ms (90.64%), Space: 45.4 MB (97.64%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,29 +1,27 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n=nums.length;
         List<List<Integer>> result = new ArrayList<>();
         if (nums == null && nums.length < 4) return result;
-        Arrays.sort(nums);  
-        for (int i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i - 1] == nums[i]) continue;
-            for (int j = i + 1; j < nums.length - 2; j++) {
+        Arrays.sort(nums); 
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
                 if (j > i + 1 && nums[j - 1] == nums[j]) continue;
-
-                int l = j + 1;
-                int k = nums.length - 1;
-
-                while (l < k) {
-                   long sum =(long) nums[i] + nums[j] + nums[l] + nums[k];
-
-                    if (sum == target) {
-                        result.add(Arrays.asList(nums[i], nums[j], nums[l], nums[k]));
-                        l++;
-                        k--;
-                        while (l < k && nums[l] == nums[l - 1]) l++;  
-                        while (l < k && nums[k] == nums[k + 1]) k--;  
-                    } else if (sum < target) {
-                        l++; 
-                    } else {
-                        k--;  
+                int k=j+1;
+                int l=n-1;
+                while(k<l){
+                    long sum=(long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum==target){
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
+                    } else if(sum>target){
+                        l--;
+                    } else{
+                        k++;
                     }
                 }
             }
