8decfb71c7710458247ca004cce563987134a8ce
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-07-23 11:15:46 +0530

    Time: 0 ms (100%), Space: 42.3 MB (70.11%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,12 +1,13 @@
 class Solution {
     public int reverseBits(int n) {
-        if(n==0) return 0;
-        int ans=0;
-        for(int i=1;i<=32;i++){
-            ans<<=1;
-            ans|=(n&1);
-            n>>=1;
-        }
-        return ans;
+        // if(n==0) return 0;
+        // int ans=0;
+        // for(int i=1;i<=32;i++){
+        //     ans<<=1;
+        //     ans|=(n&1);
+        //     n>>=1;
+        // }
+        // return ans;
+        return Integer.reverse(n);
     }
 }
