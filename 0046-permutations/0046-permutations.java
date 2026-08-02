790a2e7f5321f7fefe6633da03c7d9fbfd23e865
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-02 18:48:04 +0530

    Time: 2 ms (33.78%), Space: 45.7 MB (15.73%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,31 +1,30 @@
 class Solution {
-    List<List<Integer>> res;
-
+    List<List<Integer>> res = new ArrayList<>();
+    HashSet<Integer> set = new HashSet<>();
+    int n;
     public List<List<Integer>> permute(int[] nums) {
-        res = new ArrayList<>();
-        int n = nums.length;
-        solve(0, nums);
+        n = nums.length;
+        solve(new ArrayList<>(), nums);
         return res;
     }
 
-    void solve(int idx, int[] nums) {
-        if (idx == nums.length) {
-            List<Integer> temp = new ArrayList<>();
-            for (int num : nums)
-                temp.add(num);
-            res.add(temp);
+    void solve(List<Integer> temp, int[] nums) {
+        if (temp.size() == n) {
+            res.add(new ArrayList<>(temp));
             return;
         }
-        for (int i = idx; i < nums.length; i++) {
-            swap(nums, i, idx);
-            solve(idx + 1, nums);
-            swap(nums, i, idx);
-        }
-    }
+        for (int i = 0; i < n; i++) {
 
-    void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+            if (!set.contains(nums[i])) {
+
+                temp.add(nums[i]);
+                set.add(nums[i]);
+
+                solve(temp, nums);
+
+                set.remove(nums[i]);
+                temp.remove(temp.size() - 1);
+            }
+        }
     }
 }
