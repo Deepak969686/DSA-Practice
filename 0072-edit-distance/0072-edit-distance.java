95475f6c1b64f423a81bc8247f4d3a34481c0f21
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-10 18:23:49 +0530

    Time: 5 ms (66.38%), Space: 47 MB (84.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,20 +4,18 @@ class Solution {
     public int minDistance(String word1, String word2) {
         m=word1.length();
         n=word2.length();
-        dp=new int[m][n];
-        for(int[] rows:dp) Arrays.fill(rows,-1);
-        return solve(0,0,word1,word2);
-    }
-    int solve(int i,int j,String s1,String s2){
-        if(i==m) return n-j;
-        if(j==n) return m-i;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return solve(i+1,j+1,s1,s2);
+        dp=new int[m+1][n+1];
+        for(int i=0;i<=m;i++) dp[i][0]=i;
+        for(int j=0;j<=n;j++) dp[0][j]=j;
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(word1.charAt(i-1)==word2.charAt(j-1)){
+                    dp[i][j]=dp[i-1][j-1];
+                } else{
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
         }
-        int insert=1+solve(i,j+1,s1,s2);
-        int delete=1+solve(i+1,j,s1,s2);
-        int replace=1+solve(i+1,j+1,s1,s2);
-        return dp[i][j]= Math.min(insert,Math.min(delete,replace));
+        return dp[m][n];
     }
 }
