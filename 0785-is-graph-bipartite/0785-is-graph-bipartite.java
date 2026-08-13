2388ef106f17940a568b000713454152916887f7
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-13 09:57:11 +0530

    Time: 0 ms (100%), Space: 47.3 MB (38.28%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color,-1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!dfs(i, 0, color, graph)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean dfs(int node, int col,int[] color, int[][] graph) {
+        color[node] = col;
+        for (int nbr : graph[node]) {
+            if (color[nbr] == -1) {
+                if (!dfs(nbr, 1 - col, color, graph)) {
+                    return false;
+                }
+            }
+            else if (color[nbr] == col) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
