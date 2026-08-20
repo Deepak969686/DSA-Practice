fb6f622bd6535588eba26a193c0f20cd1688065d
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-20 23:24:08 +0530

    Time: 9 ms (24.02%), Space: 47.3 MB (65.35%) - LeetHub

---
+++ 3446-sort-matrix-by-diagonals/3446-sort-matrix-by-diagonals.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[][] sortMatrix(int[][] mat) {
+        Map<Integer, PriorityQueue<Integer>> map = new HashMap<>();
+        int m = mat.length;
+        int n = mat[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i-j<0){
+                map.putIfAbsent(i - j, new PriorityQueue<>());
+                map.get(i - j).add(mat[i][j]);
+                } else{
+                map.putIfAbsent(i - j, new PriorityQueue<>(Collections.reverseOrder()));
+                map.get(i - j).add(mat[i][j]);
+                }
+            }
+        }
+
+        int[][] ans = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans[i][j] = map.get(i - j).poll();
+            }
+        }
+        return ans;
+    }
+}
