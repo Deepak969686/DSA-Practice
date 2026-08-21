afb78bc71127073c74736523fd9b3fbd40cc291c
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-22 00:25:45 +0530

    Time: 0 ms (100%), Space: 42.8 MB (86.83%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -4,20 +4,19 @@ class Solution {
     public int numDecodings(String s) {
         n=s.length();
         dp=new int[n+1];
-        Arrays.fill(dp,-1);
-      return solve(0,s);  
-    }
-    int solve(int i,String s){
-        if(i==n) return 1;
-        if(s.charAt(i)=='0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int singlechar=solve(i+1,s);
-        int doublechar=0;
-        if(i+1<n){
-            if(s.charAt(i)=='1' || (s.charAt(i)=='2' && s.charAt(i+1)<='6')){
-                doublechar=solve(i+2,s);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s.charAt(i)=='0') dp[i]=0;
+            else{
+                dp[i]=dp[i+1];
+                if(i+1<n){
+                    if(s.charAt(i)=='1' || (s.charAt(i)=='2' && s.charAt(i+1)<='6')){
+                        dp[i]+=dp[i+2];
+                    }
+                }
             }
         }
-        return dp[i]=singlechar+doublechar;
+      return dp[0];  
     }
+
 }
