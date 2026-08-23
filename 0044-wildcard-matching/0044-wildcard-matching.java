7d8e11ad2bb35668377c5ec37d7d5b2d54a54ed4
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-08-23 12:45:50 +0530

    Time: 16 ms (72.18%), Space: 64.2 MB (6.81%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,35 @@
+class Solution {
+    int m, n;
+    Boolean[][] dp;
+    public boolean isMatch(String s, String p) {
+        m = s.length();
+        n = p.length();
+        dp=new Boolean[m][n];
+        return solve(m - 1, n - 1, s, p);
+    }
+    boolean solve(int i, int j, String s, String p) {
+        if (i < 0 && j < 0) {
+            return true;
+        }
+        if (j < 0) {
+            return false;
+        }
+        if (i < 0) {
+            for (int k = 0; k <= j; k++) {
+                if (p.charAt(k) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        if(dp[i][j]!=null) return dp[i][j];
+        if (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?') {
+            return dp[i][j]= solve(i - 1, j - 1, s, p);
+        }
+        if (p.charAt(j) == '*') {
+            return dp[i][j]= solve(i - 1, j, s, p) || solve(i, j - 1, s, p);
+        }
+
+        return  false;
+    }
+}
