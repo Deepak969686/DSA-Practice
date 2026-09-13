984c9c2a7af883abf30ea3772fc808d299866e5d
Deepak Kumar Saini <157050176+Deepak969686@users.noreply.github.com>
2026-09-13 14:18:43 +0530

    Time: 8 ms (33.89%), Space: 46.7 MB (74.58%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.java
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -1,22 +1,24 @@
 class Solution {
-    int[] sum;
-    int[] dp;
     public int deleteAndEarn(int[] nums) {
-        int maxlen=0;
-        for(int num:nums) maxlen=Math.max(maxlen,num);
-        dp=new int[maxlen+1];
-        Arrays.fill(dp,-1);
-        sum=new int[maxlen+1];
-        for (int num : nums) {
-            sum[num] += num;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num,0) + 1);
         }
-        return solve(0, maxlen);
-    }
-    int solve(int i,int maxlen){
-        if(i>maxlen) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int skip = solve(i + 1, maxlen);
-        int take = sum[i] + solve(i + 2, maxlen);
-        return dp[i]= Math.max(take, skip);
+        int maxlen = 0;
+        for(int num : nums){
+            maxlen = Math.max(maxlen, num);
+        }
+        int[] sum = new int[maxlen + 1];
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            int num = entry.getKey();
+            int freq = entry.getValue();
+            sum[num] = num * freq;
+        }
+        int[] dp = new int[maxlen + 1];
+        dp[1] = sum[1];
+        for(int i = 2; i <= maxlen; i++){
+            dp[i] = Math.max(dp[i-1],sum[i] + dp[i-2]);
+        }
+        return dp[maxlen];
     }
 }
